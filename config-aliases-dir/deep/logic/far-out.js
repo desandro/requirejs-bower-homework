1ba180321f90edfa62d1bf141806aabac4be790c
David DeSandro <desandrocodes@gmail.com>
2013-07-16 22:07:59 -0400

    add config-aliases-dir; Ref #1

---
+++ config-aliases-dir/deep/logic/far-out.js
@@ -0,0 +1,12 @@
+requirejs.config({
+  paths: {
+    greeter: '../../components/greeter',
+    recipient: '../../components/recipient'
+  }
+});
+
+requirejs( [ 'greeter/greeter' ], function( Greeter ) {
+
+  new Greeter();
+
+});
